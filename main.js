13d9dd79cb0e3ad7f81805b7c3f07acb4d87fb5f
Laith-Samamah <Laith.samamah@gmail.com>
2022-10-31 00:04:04 +0300

    add

---
+++ main.js
@@ -0,0 +1,44 @@
+//Q1
+// function myFunction(num) {
+//   if (num <= 0) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// myFunction(-4);
+
+//************************************************************** */
+//Q2
+// function next(num) {
+//   console.log(num + 1);
+// }
+// next(4);
+
+//************************************************************** */
+
+//Q3
+// fArr = [];
+// Arr = ["laith", "num9", "num4", "num5", "num6", "num7", "ali"];
+// function strnum(array) {
+//   array.forEach((element) => {
+//     for (i = 0; i < element.length; i++) {
+//       if (isNaN(element[i])) {
+//         continue;
+//       } else {
+//         fArr.push(element);
+//       }
+//     }
+//   });
+//   console.log(fArr);
+// }
+
+// strnum(Arr);
+
+//************************************************************** */
+
+// Q4
+function check(num) {
+  if (num % 2 == 0) {
+  }
+}
